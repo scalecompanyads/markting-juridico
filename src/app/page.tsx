dcc67b654c1b8293d4ae8bfd8ab1c8eedba97bc0
paulojrzti <paulojrzti@gmail.com>
2026-06-15 15:43:35 -0300

    feat: add solution and testimonials to home page, inject GTM script on scale-advogados

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,10 @@ import Navbar from "@/components/Navbar";
 import Hero from "@/components/Hero";
 import LogosMarquee from "@/components/LogosMarquee";
 import Problem from "@/components/Problem";
+import Solution from "@/components/Solution";
 import ExpertiseSEO from "@/components/ExpertiseSEO";
 import Team from "@/components/Team";
+import Testimonials from "@/components/Testimonials";
 import About from "@/components/About";
 import FAQ from "@/components/FAQ";
 import { FinalCTA, Footer } from "@/components/Footer";
@@ -16,8 +18,10 @@ export default function Home() {
         <Hero />
         <LogosMarquee />
         <Problem />
+        <Solution />
         <ExpertiseSEO />
         <Team />
+        <Testimonials />
         <About />
         <FAQ />
         <FinalCTA />
